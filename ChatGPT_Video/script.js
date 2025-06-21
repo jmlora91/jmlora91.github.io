34ec385de870837761b73cdde1179bd70559568d
jmlora <josmanuel.loramora+github@gmail.com>
2025-06-21 19:26:42 +0200

    .

--- ChatGPT_Video/script.js
+++ ChatGPT_Video/script.js
@@ -16,51 +16,24 @@ function iniciarChat() {
         msg.classList.add(mensajes[index].autor);
 
         if (mensajes[index].autor === 'chatgpt') {
-            msg.innerHTML = 'ChatGPT is typing<span class="cursor"></span>';
+            msg.innerText = 'ChatGPT is typing...';
             document.getElementById('chatContainer').appendChild(msg);
-            scrollToBottom();
             setTimeout(() => {
-                mostrarTextoLetraPorLetra(msg, mensajes[index].mensaje, () => {
-                    msg.classList.add('visible');
-                    index++;
-                    setTimeout(iniciarChat, 1000);
-                });
+                msg.innerText = mensajes[index].mensaje;
+                msg.classList.add('visible');
+                index++;
+                setTimeout(iniciarChat, mensajes[index - 1].tiempo);
             }, 1000);
         } else {
             msg.innerText = mensajes[index].mensaje;
             document.getElementById('chatContainer').appendChild(msg);
-            setTimeout(() => { msg.classList.add('visible'); scrollToBottom(); }, 50);
+            setTimeout(() => { msg.classList.add('visible'); }, 50);
             setTimeout(() => {
                 index++;
                 iniciarChat();
             }, mensajes[index].tiempo);
         }
     }
-}
-
-function mostrarTextoLetraPorLetra(elemento, texto, callback) {
-    let i = 0;
-    elemento.innerHTML = '';
-
-    let cursor = document.createElement('span');
-    cursor.classList.add('cursor');
-    elemento.appendChild(cursor);
-
-    let interval = setInterval(() => {
-        cursor.before(texto.charAt(i));
-        i++;
-        scrollToBottom();
-        if (i >= texto.length) {
-            clearInterval(interval);
-            cursor.remove();
-            if (callback) callback();
-        }
-    }, 30);
-}
-
-function scrollToBottom() {
-    let chatContainer = document.getElementById('chatContainer');
-    chatContainer.scrollTop = chatContainer.scrollHeight;
 }
 
 window.onload = cargarConversacion;
